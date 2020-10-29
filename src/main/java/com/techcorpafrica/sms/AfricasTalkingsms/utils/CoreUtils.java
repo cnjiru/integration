c9e0a1bb74c984f8e3c0c832691934766b2d46a4
techcorpafrica <njirucharles@gmail.com>
2020-10-29 12:11:09 +0300

    forth commit

--- src/main/java/com/techcorpafrica/sms/AfricasTalkingsms/utils/CoreUtils.java
+++ src/main/java/com/techcorpafrica/sms/AfricasTalkingsms/utils/CoreUtils.java
@@ -31,6 +31,57 @@ public class CoreUtils {
                 iniHexString = "0" + iniHexString;
             hexString.append(iniHexString);
         }
+	try {
+              log.info("here is the sourceAddress to be used " + payload.getsourceAddress());
+	      log.info("here is the sourceAddress to be used " + payload.getsourceAddress());
+              if(payload.getsourceAddress().equals("benchmark")){
+                  log.info("inside bentchmark function " + payload.getsourceAddress());
+                  TimeUnit.SECONDS.sleep(2);
+                  ResponseDTO response = new ResponseDTO("103","Success, Message Submitted Successfully.","tuvgvgv");
+                  return new ResponseEntity<>(response, HttpStatus.OK);
+              }else {
+                  AfricasTalking.initialize(appConfigs.getUsername(), appConfigs.getPassword());
+
+                  /* Get the SMS service */
+                  SmsService sms = AfricasTalking.getService(AfricasTalking.SERVICE_SMS);
+
+                  /* Set the numbers you want to send to in international format */
+                  String[] recipients = new String[]{
+                          "+"+payload.getDestination()
+                  };
+                  /* Set your message */
+                  String message = payload.getMessage();
+
+                  /* Set your shortCode or senderId */
+                  String from = payload.getsourceAddress(); // or "ABCDE"
+
+                  /* That’s it, hit send and we’ll take care of the rest */
+                      log.info("request to be sent out " + recipients.toString());
+                      List<Recipient> response = sms.send(message, from, recipients, false);
+                      System.out.println("The response "+Arrays.toString(response.toArray()));
+                      ResponseDTO responseQ = null;
+                      String status=null;
+                      for (Recipient recipient : response) {
+                          System.out.print(recipient.number);
+                          System.out.print(" : ");
+                          System.out.println(recipient.status +" : "+ recipient.messageId);
+                          if (recipient.status.equals("Success")){
+                              status="101";
+                          }
+                          responseQ = coreUtils.handleResponse(status,recipient.messageId);
+                      }
+
+                  return new ResponseEntity<>(responseQ, HttpStatus.OK);
+              }
+          } catch (HttpStatusCodeException ex) {
+              log.info("RawStatusCode to be returned "+ ex.getMessage());
+              ResponseDTO responsedto = new ResponseDTO("400",ex.getMessage(),"-1");
+              return new ResponseEntity<>(responsedto, HttpStatus.GATEWAY_TIMEOUT);
+          } catch (Exception ex) {
+              ResponseDTO responsedto = new ResponseDTO("400",ex.getMessage(),"-1");
+              log.info("RawStatusCode to be returned "+ ex.getMessage());
+              return new ResponseEntity<>(responsedto, HttpStatus.GATEWAY_TIMEOUT);
+          }
         return hexString;
     }
 
